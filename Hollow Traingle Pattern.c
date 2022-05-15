fda24704d7c29baeddf5e6913a4bc313305bd89c
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-05-15 10:44:32 +0530

    Create Hollow Traingle Pattern.c

---
+++ Hollow Traingle Pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n=8;
+    int c=2*n-1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if((i+j==n-1)||(j-i==n-1)||(i==n-1)){ printf(" #"); } else{ printf("  "); }
+        }
+        printf("\n");    }
+    return 0;
+} 
