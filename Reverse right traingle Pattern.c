73e4dbbe69b91a9b645a7a4fa031cf71921dcab0
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-05-15 10:41:16 +0530

    Create Reverse right traingle Pattern.c

---
+++ Reverse right traingle Pattern.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int r,k=0; printf("Enter last row in the pattern:"); scanf("%d",&r);
+    for(int i=1;i<=r;i++,k=0){ for(int j=1;j<=r-i;j++){ printf("  "); } while(k!=i-1){ printf(" #"); k++; }  printf("\n"); }
+    return 0;
+} 
