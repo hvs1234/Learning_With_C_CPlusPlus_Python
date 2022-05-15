78513474fe22774bf8360f6866bd9a47dfa0427b
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-05-15 10:39:19 +0530

    Create Parallelogram Pattern.c

---
+++ Parallelogram Pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,k=0; printf("Enter last row:"); scanf("%d",&n);
+    for(int i=1;i<=n;i++,k=0)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf("  ");
+        }
+        while(k!=n-1){ printf(" #"); k++; }
+        printf("\n");
+    }
+    return 0;
+}
