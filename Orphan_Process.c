2f3471eabb3844f05c8f5e6eebdb44be292b65db
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-10-08 11:29:59 +0530

    Create Orphan_Process.c

---
+++ Orphan_Process.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+    pid_t p = fork();
+    if(p==0)
+    {
+        sleep(2);
+        printf("I am a child having pid is: %d\n",getpid());
+        printf("My parent pid is: %d\n",getppid());
+    }
+    else
+    {
+        printf("I am a parent having pid is: %d\n",getpid());
+        printf("My child pid is: %d\n",p);
+    }
+    return 0;
+}
