4f2c6b9260ffe3f05e209732d4eef5133f26caac
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-10-09 09:35:21 +0530

    Update Newton_Rasphon_Method.c

--- Newton_Rasphon_Method.c
+++ Newton_Rasphon_Method.c
@@ -1,32 +1,25 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
-float f(float x)
-{
-    return x*x*x-2*x-5;
-}
-float df (float x)
-{
-    return 3*x*x-2;
-}
+#define e 0.001
+#define f(x) x*x*x-4*x+1
+#define df(x) 3*x*x-4
+
 int main()
 {
-    int itr, maxmitr;
-    float h, x0, x1, allerr;
-    printf("\nEnter x0, allowed error and maximum iterations\n");
-    scanf("%f %f %d", &x0, &allerr, &maxmitr);
-    for (itr=1; itr<=maxmitr; itr++)
-    {
-        h=f(x0)/df(x0);
-        x1=x0-h;
-        printf(" At Iteration no. %3d, x = %9.6f\n", itr, x1);
-        if (fabs(h) < allerr)
-        {
-            printf("After %3d iterations, root = %8.6f\n", itr, x1);
-            return 0;
-        }
-        x0=x1;
-    }
-    printf(" The required solution does not converge or iterations are insufficient\n");
-    return 0;
+	int i=0;
+	float x0,x1,f0,f1,df0;
+	printf("Enter the value of x: "); scanf("%f",&x0);
+    do
+	{
+		f0 = f(x0);
+		df0 = df(0);
+		x1 = x0 - (f0/df0);
+		f1 = f(x1);
+		x0 = x1;
+		i++;
+		printf("No. of iterations are: %d\t",i);
+		printf("Root are: %f\t",x1);
+        printf("Value of function are: %f\n",f1);
+	} while (fabs(f1)>e);
+	return 0;
 }
