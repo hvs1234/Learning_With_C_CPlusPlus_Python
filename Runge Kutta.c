286a93f1c0eb16bb7a6122093fb0a52c3270d712
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-12-07 11:05:35 +0530

    Create Runge Kutta.c

---
+++ Runge Kutta.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+float f(float x,float y);
+int main()
+{
+    float x0,y0,m1,m2,m3,m4,m,y,x,h,xn;
+    printf("Enter x0,y0,xn,h:");
+    scanf("%f %f %f %f",&x0,&y0,&xn,&h);
+    x=x0;
+    y=y0;
+    printf("\n\nX\t\tY\n");
+    while(x<xn)
+    {
+        m1=f(x0,y0);
+        m2=f((x0+h/2.0),(y0+m1*h/2.0));
+        m3=f((x0+h/2.0),(y0+m2*h/2.0));
+        m4=f((x0+h),(y0+m3*h));
+        m=((m1+2*m2+2*m3+m4)/6);
+        y=y+m*h;
+        x=x+h;
+        printf("%f\t%f\n",x,y);
+    }
+}
+float f(float x,float y)
+{
+    float m;
+    m=(x-y)/(x+y);
+    return m;
+}
